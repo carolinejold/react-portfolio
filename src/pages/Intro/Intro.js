cfe3c271d6384781ae4d5d48310a19f99933f675
Caroline Old <carolinejold@gmail.com>
2020-10-26 20:20:57 +0000

    worked on intro

--- src/pages/Intro/Intro.js
+++ src/pages/Intro/Intro.js
@@ -5,31 +5,42 @@ const styles = {
   container: {
     fontFamily: "PT Mono, monospace",
     backgroundColor: "black",
+    backgroundImage:
+      'url("https://images.pexels.com/photos/97077/pexels-photo-97077.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260")',
+    backgroundPosition: "center",
+    backgroundSize: "100em",
     color: "whitesmoke",
     height: "100vh",
     textAlign: "center",
-    paddingTop: "4em",
   },
+  cover: {
+    background: 'black',
+    opacity: '0.7',
+    height: '90vh',
+    paddingTop: '4em'
+  }
 };
 
 const Intro = () => {
   return (
     <div style={styles.container}>
-      <h1>CAROLINE OLD</h1>
-      <br></br>
-      <h3>
-        Full-stack JavaScript developer with a passion for front-end web
-        development
-      </h3>
-      <br></br>
-      <p>Want to learn more?</p>
-      <br></br>
-      <Link to="about">About</Link>
-      <br></br>
-      <Link to="projects">Projects</Link>
-      <br></br>
-      <Link to="contact">Contact</Link>
-      <br></br>
+      <div style={styles.cover}>
+        <h1 style={{fontSize: '6em'}}>CAROLINE OLD</h1>
+        <br></br>
+        <h3 style={{margin: 'auto', maxWidth: '14em'}}>
+          Full-stack JavaScript developer with a passion for front-end web
+          development
+        </h3>
+        <br></br>
+        <p>Want to learn more?</p>
+        <br></br>
+        <Link to="about">About</Link>
+        <br></br>
+        <Link to="projects">Projects</Link>
+        <br></br>
+        <Link to="contact">Contact</Link>
+        <br></br>
+      </div>
     </div>
   );
 };
